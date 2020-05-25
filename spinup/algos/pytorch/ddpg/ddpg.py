418bbb6b8217fc7b6b170097a160df7c4c2a5276
Matteo <matteo.papini@polimi.it>
2020-05-25 19:20:01 +0200

    minor fix

--- spinup/algos/pytorch/ddpg/ddpg.py
+++ spinup/algos/pytorch/ddpg/ddpg.py
@@ -278,7 +278,7 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         # End of trajectory handling
         if d or (ep_len == max_ep_len):
             logger.store(EpRet=ep_ret, EpLen=ep_len)
-            o, ep_ret, ep_len, ep_info = env.reset(), 0, 0, 0
+            o, ep_ret, ep_len = env.reset(), 0, 0
 
         # Update handling
         if t >= update_after and t % update_every == 0:
@@ -289,6 +289,7 @@ def ddpg(env_fn, actor_critic=core.MLPActorCritic, ac_kwargs=dict(), seed=0,
         # End of epoch handling
         if (t+1) % steps_per_epoch == 0:
             epoch = (t+1) // steps_per_epoch
+            ep_info = 0
 
             # Save model
             if (epoch % save_freq == 0) or (epoch == epochs):
